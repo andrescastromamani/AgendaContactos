819cf42682b55db941d69fb372e35fba06076a4e
Andres <castroandresrm@gmail.com>
2021-02-02 10:25:26 -0400

    Agregamo la estructura inicial del proyecto

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include('includes/layouts/header.php');?>
+    <div class="contenedor-barra">
+        <h1>Agenda de Contactos</h1>
+    </div>
+<?php include('includes/layouts/footer.php');?>
