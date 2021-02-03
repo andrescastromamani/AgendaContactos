09426ad2e905c64bdaa27603dda4ac33292c9170
Andres <castroandresrm@gmail.com>
2021-02-03 12:39:41 -0400

    Mostramos los nuevos registros en el DOM y la Notificacion de Correcto

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const formularioContactos = document.querySelector('#contacto');
+const formularioContactos = document.querySelector('#contacto'),
+      listadoContactos = document.querySelector('#lista-contactos tbody');
  eventLinteners();
 function eventLinteners(){
     //cuando el formulario de editar o crear se ejecuta
@@ -48,7 +49,48 @@ function insertarBD(datos){
         if(this.status === 200){
             console.log(JSON.parse(xhr.responseText));
             const respuesta = JSON.parse(xhr.responseText);
-            console.log(respuesta.empresa);
+            //insertar un nuevo elemento en la Tabla
+            const nuevoContacto = document.createElement('tr');
+            nuevoContacto.innerHTML = `
+                <td>${respuesta.datos.nombre}</td>
+                <td>${respuesta.datos.empresa}</td>
+                <td>${respuesta.datos.telefono}</td>
+            `;
+
+            //creamos el contenedor para los botones
+            const contenedorAcciones = document.createElement('td');
+            //creamos el icono de editar
+            const iconoEditar = document.createElement('i');
+            iconoEditar.classList.add('fas','fa-pen-square');
+            //creamos el enlace para editar
+            const btnEditar = document.createElement('a');
+            btnEditar.appendChild(iconoEditar);
+            btnEditar.href = `editar.php?id=${respuesta.datos.id_insertado}`;
+            btnEditar.classList.add('btn','btn-editar');
+            //Agregar al contenedor acciones
+            contenedorAcciones.appendChild(btnEditar);
+
+            //Creamos el icono eliminar
+            const iconoEliminar = document.createElement('i');
+            iconoEliminar.classList.add('fas','fa-trash');
+            //creamos el btn eliminar
+            const btnEliminar = document.createElement('button');
+            btnEliminar.appendChild(iconoEliminar);
+            btnEliminar.setAttribute('data-id',respuesta.datos.id_insertado);
+            btnEliminar.classList.add('btn','btn-eliminar');
+            //agregamos al contenedor de acciones
+            contenedorAcciones.appendChild(btnEliminar);
+            //Agregamos a nuevo contacto
+            nuevoContacto.appendChild(contenedorAcciones);
+
+            //agregamos a la lista de contactos
+            listadoContactos.appendChild(nuevoContacto);
+
+            //resetear formulario
+            document.querySelector('form').reset();
+            //mostrar notificacion
+            mostrarNotificacion('Se Agrego correctamente','correcto');
+
         }
     }
     //enviar los datos
